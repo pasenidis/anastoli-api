7ff941d9387161d94eb603706b385fadfa815b77
Edward Pasenidis <me@edpasenidis.tech>
2020-10-06 18:53:39 +0300

    fix: loop over every page

--- src/scrapper/main.py
+++ src/scrapper/main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import requests
 import json
-from .config import ENDPOINT
+from .config import ENDPOINT, ROUNDS
 
 
 def main():
@@ -11,24 +11,28 @@ def main():
 
 def total_schools():
     def work():
-        html = urlopen(ENDPOINT)
-        bs = BeautifulSoup(html.read(), 'html.parser')
-
-        number = bs.find('div', {'class': {'summary'}})
-        total = number.find_all('b')
-
         schools = []
-        for i, school in enumerate(bs.find_all('tr', {'class': 'w0'})):
-            x = school.find_all('td', {'class': 'w0'})
-            sc = {
-                'id': i,
-                'name': x[0].text,
-                'region': x[1].text,
-                'address': x[2].text,
-                'closed_until': x[3].text,
-                'comments': x[4].text
-            }
-            schools.append(sc)
+        c = 0
+        
+        for i in range(ROUNDS):
+            html = urlopen(f'{ENDPOINT}/index.php?page={i + 1}&per-page=100')
+            bs = BeautifulSoup(html.read(), 'html.parser')
+
+            number = bs.find('div', {'class': {'summary'}})
+            total = number.find_all('b')
+
+            for i, school in enumerate(bs.find_all('tr', {'class': 'w0'})):
+                x = school.find_all('td', {'class': 'w0'})
+                c += 1
+                sc = {
+                    'id': c,
+                    'name': x[0].text,
+                    'region': x[1].text,
+                    'address': x[2].text,
+                    'closed_until': x[3].text,
+                    'comments': x[4].text
+                }
+                schools.append(sc)
 
         return (int(total[1].text), schools)
 
